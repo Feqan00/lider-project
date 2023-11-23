6c2d0153e59cb9559022b4964e87c0e0f3ae0a9c
Feqan00 <ehmedovfeqan18@gmail.com>
2023-11-23 14:00:31 +0400

    burger-menu

--- home.js
+++ home.js
@@ -1,9 +1,11 @@
 const dropdown = document.querySelector('.dropdown-menu')
 const ironClick = document.querySelector('.iron-click')
+const burgerMenu = document.querySelector('.burger-menu')
 
 ironClick.addEventListener('click', () => {
     dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-})  
+})
 
-
-
+burgerMenu.addEventListener('click', () => {
+    
+})

803ccdcd32768a471400aa3a958f20cdc96cddbf
Feqan00 <ehmedovfeqan18@gmail.com>
2023-11-24 13:14:57 +0400

    new-changes

--- home.js
+++ home.js
@@ -1,11 +1,18 @@
 const dropdown = document.querySelector('.dropdown-menu')
 const ironClick = document.querySelector('.iron-click')
 const burgerMenu = document.querySelector('.burger-menu')
+const clickBurgerMenu = document.querySelector('.burger_menu-in')
+const clickCloseburger = document.querySelector('.close-button')
 
 ironClick.addEventListener('click', () => {
     dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
 })
 
 burgerMenu.addEventListener('click', () => {
+    clickBurgerMenu.style.display = (clickBurgerMenu.style.display === "block") ? "none" : "block";
+})
+
+
+clickCloseburger.addEventListener('click', () => {
     
 })
